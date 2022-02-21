ea7704e54d1dad9d5b1543680a525a3626beda41
JakeZimski <jakegarratt02@gmail.com>
2022-02-21 16:01:09 +0800

    Implemented an animation for a door and a door frame Implemented the core functionality for a health system, including a simple health bar Revised grounding of the player and implemented a crouch functionality

---
+++ class learning/Assets/Health.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth;
+    public Image healthBar;
+
+    private float currentHealth = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.fillAmount = currentHealth / maxHealth;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        if (currentHealth > 0)
+        {
+            currentHealth -= amount;
+            Debug.Log("current health is now" + currentHealth);
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+                // we perish
+            }
+            // subtract health from health bar
+            healthBar.fillAmount = currentHealth / maxHealth;
+        }
+        Debug.Log("Took Damage");
+    }
+}
